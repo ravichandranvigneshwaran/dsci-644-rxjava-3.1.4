46117c2350cdfe98a643529f137f9427cf4690a7
Vigneshwaran Ravichandran <vr9965@rit.edu>
2022-04-04 00:01:00 -0400

    third defect fixed

--- src/main/java/io/reactivex/rxjava3/internal/operators/observable/ObservableWindowBoundarySelector.java
+++ src/main/java/io/reactivex/rxjava3/internal/operators/observable/ObservableWindowBoundarySelector.java
@@ -72,8 +72,6 @@ public final class ObservableWindowBoundarySelector<T, B, V> extends AbstractObs
         final AtomicBoolean downstreamDisposed;
 
         final AtomicLong requested;
-        //FIXME: UuF: Unused variable (Efficiency)
-        long emitted;
 
         volatile boolean upstreamCanceled;
 
